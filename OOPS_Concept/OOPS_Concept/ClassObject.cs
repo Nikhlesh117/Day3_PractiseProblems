1ef64282a7293d29061addf9b223596302567a2a
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-04-29 22:15:10 +0530

    Added: Class And Object

---
+++ OOPS_Concept/OOPS_Concept/ClassObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OOPS_Concept
+{
+    public class ClassObject
+    {
+        public void person()
+        {
+            Console.WriteLine("Enter the your Name :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hi, my name is " + name + " and I am " + age + " years old.");
+
+        }
+    }
+}
---
+++ OOPS_Concept/OOPS_Concept/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OOPS_Concept
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //ClassAndObject
+            ClassObject classObject = new ClassObject();
+            classObject.person();
+        }
+    }
+}
