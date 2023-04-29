c8cb547625e2116db60c3dd289d4d68d3ca54a67
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-04-29 22:37:34 +0530

    Added: Polymorphism

---
+++ OOPS_Concept/OOPS_Concept/Polymorphism.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Concept
+{
+    public class Fruit
+    {
+        public void PrintName()
+        {
+            Console.WriteLine("Fruit");
+        }
+    }
+    public class Apple : Fruit
+    {
+        public void PrintName()
+        {
+            Console.WriteLine("Apple fruit");
+        }
+    }
+}
--- OOPS_Concept/OOPS_Concept/Program.cs
+++ OOPS_Concept/OOPS_Concept/Program.cs
@@ -5,8 +5,14 @@
         public static void Main(string[] args)
         {
             //ClassAndObject
-            ClassObject classObject = new ClassObject();
-            classObject.person();
+            //ClassObject classObject = new ClassObject();
+            //classObject.person();
+
+            //Polymorphism
+            Fruit obj1= new Fruit();
+            obj1.PrintName();
+            Apple obj2= new Apple();
+            obj2.PrintName();
         }
     }
 }
