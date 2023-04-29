5761b13b6bf8cbce4f79365647343a6f075337c7
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-04-29 22:55:15 +0530

    Added: Overloading

---
+++ OOPS_Concept/OOPS_Concept/Overloading.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Concept
+{
+    public class Overloading
+    {
+        public int Add(int num1, int num2)
+        {
+            return (num1 + num2);
+        }
+        public int Add(int num1, int num2, int num3)
+        {
+            return (num1 + num2 + num3);
+        }
+    }
+}
--- OOPS_Concept/OOPS_Concept/Program.cs
+++ OOPS_Concept/OOPS_Concept/Program.cs
@@ -9,10 +9,15 @@
             //classObject.person();
 
             //Polymorphism
-            Fruit obj1= new Fruit();
-            obj1.PrintName();
-            Apple obj2= new Apple();
-            obj2.PrintName();
+            //Fruit obj1= new Fruit();
+            //obj1.PrintName();
+            //Apple obj2= new Apple();
+            //obj2.PrintName();
+
+            //Overloading
+            Overloading overloading= new Overloading();
+            Console.WriteLine("Add with two int parameter :" + overloading.Add(4, 2));
+            Console.WriteLine("Add with three int parameter :" + overloading.Add(2, 2, 8));
         }
     }
 }
